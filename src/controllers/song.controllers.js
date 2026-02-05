e23176d36e966dd56af795e0379474dbf063fa77
Juan Ferreyra <juanitouu.f@gmail.com>
2026-02-05 20:23:03 -0300

    feat: agrega funcion createSong para guardar canciones

--- src/controllers/song.controllers.js
+++ src/controllers/song.controllers.js
@@ -8,3 +8,21 @@ export const getSongs = async (req, res) => {
         return res.status(500).json({message: "Error al obtener canciones"})
     }
 }
+
+export const createSong = async (req, res) => {
+    try {
+        const {title, artist, image, youtubeUrl, duration} = req.body;
+        const newSong = new Song({
+            title,
+            artist,
+            image,
+            youtubeUrl,
+            duration,
+            user: req.user.id
+        });
+        const savedSong = await newSong.save(); 
+        res.status(201).json(savedSong);
+    } catch (error) {
+        return res.status(500).json({message: "Error al guardar la cancion"})
+    }
+}
