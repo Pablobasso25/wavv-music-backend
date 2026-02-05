6e79271b7a05a0176d5e52b1a92f3b33189ad3a3
Pablo Basso <pablobasso25@gmail.com>
2026-02-05 20:10:12 -0300

    agrega función de logout y profile

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -72,9 +72,24 @@ res.json({
 }
 };
 
+export const logout = (req, res) => {
+  res.cookie("token", "", {
+    expires: new Date(0),
+  });
+  return res.sendStatus(200);
+};
 
+export const profile = async (req, res) => {
+  const userFound = await User.findById(req.user.id);
+  if (!userFound)
+    return res.status(400).json({ message: "Usuario no encontrado" });
 
-
+  return res.json({
+    id: userFound._id,
+    username: userFound.username,
+    email: userFound.email,
+  });
+};
 
 
 
