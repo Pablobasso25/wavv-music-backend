2a74a2c74f317c485e89f491e62463e59084bb43
Luana Jakubowicz <luanajakubowicz@hotmail.com>
2026-02-05 20:58:53 -0300

    Busca al usuario y su playlist

--- src/controllers/playlist.controllers.js
+++ src/controllers/playlist.controllers.js
@@ -5,6 +5,13 @@ export const addSongToPlaylist = async (req, res) => {
     try {
         const userId = req.user.id;
         const { songId } = req.body;
+
+        const user = await User.findById(userId);
+        let playlist = await Playlist.findOne({ user: userId });
+
+        if (!playlist) {
+            playlist = new Playlist ({ user: userId, songs: [] });
+        }
     } catch (error) {
         
     }
