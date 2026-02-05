6635d3da4c60028ede7a08bdb9f3f9f6bb37bfb5
Pablo Basso <pablobasso25@gmail.com>
2026-02-04 21:20:24 -0300

    [WM-10] feat(auth): crear generador de tokens JWT para manejo de sesiones

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,18 +1,10 @@
 import { Router } from "express";
-import {
-  login,
-  logout,
-  profile,
-  register,
-} from "../controllers/auth.controller.js";
-import { authRequired } from "../middlewares/validateToken.js";
+import { register } from "../controllers/auth.controller.js";
 import { validateSchema } from "../middlewares/validator.middleware.js";
-import { loginSchema, registerSchema } from "../schemas/auth.schema.js";
+import { registerSchema } from "../schemas/auth.schema.js";
 
 const router = Router();
 
 router.post("/register", validateSchema(registerSchema), register);
-router.post("/login", validateSchema(loginSchema), login);
-router.post("/logout", logout);
-router.get("/profile", authRequired, profile);
+
 export default router;
