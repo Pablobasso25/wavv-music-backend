7e2182f7cc06d7dd7d890f44189164ec043f26b3
tomas <tomasgomezzi36@gmail.com>
2026-02-14 18:56:06 -0300

    feat: agregar configuración de cloudinary usando variables de entorno

--- src/config.js
+++ src/config.js
@@ -4,3 +4,7 @@ config();
 export const PORT = process.env.PORT || 4001;
 export const MONGODB_URI = process.env.MONGODB_URI;
 export const TOKEN_SECRET = process.env.TOKEN_SECRET;
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
+
