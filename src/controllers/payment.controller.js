b23b1cd503be3e8c5d8c327a7128be58ff694f03
Pablo Basso <pablobasso25@gmail.com>
2026-02-16 17:32:29 -0300

    [wm-20] feat:agrega preferencias de mercado pago

---
+++ src/controllers/payment.controller.js
@@ -0,0 +1,75 @@
+import { MercadoPagoConfig, Preference } from "mercadopago";
+import User from "../models/user.model.js"
+
+const client = new MercadoPagoConfig({
+  accessToken: process.env.MP_ACCESS_TOKEN,
+});
+
+export const createPreference = async (req, res) => {
+  try {
+    const { planType, price } = req.body;
+    const preference = new Preference(client);
+
+    const result = await preference.create({
+      body: {
+        items: [
+          {
+            title: `Wavv Music - ${planType}`,
+            quantity: 1,
+            unit_price: Number(price),
+            currency_id: "ARS",
+          },
+        ],
+        back_urls: {
+          success: "https://unnationalistic-nonapparently-edmund.ngrok-free.dev/api/payments/success",
+          failure: "https://unnationalistic-nonapparently-edmund.ngrok-free.dev/api/payments/failure",
+          pending: "https://unnationalistic-nonapparently-edmund.ngrok-free.dev/api/payments/pending",
+        },
+        notification_url: "https://unnationalistic-nonapparently-edmund.ngrok-free.dev/api/payments/webhook",
+        external_reference: String(req.user.id),
+      },
+    });
+    return res.json({ id: result.id, init_point: result.init_point });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const receiveWebhook = async (req, res) => {
+  try {
+    const { query } = req;
+    const topic = query.topic || query.type;
+
+    if (topic === "payment") {
+      const paymentId = query.id || query["data.id"];
+
+      const response = await fetch(
+        `https://api.mercadopago.com/v1/payments/${paymentId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.MP_ACCESS_TOKEN}`,
+          },
+        },
+      );
+
+      const data = await response.json();
+
+      if (data.status === "approved") {
+        const userId = data.external_reference;
+
+        const updated = await User.findByIdAndUpdate(
+          userId,
+          {
+            "subscription.status": "premium",
+            "subscription.startDate": new Date(),
+            "subscription.mp_preference_id": data.id,
+          },
+          { new: true }
+        );
+      }
+    }
+    res.sendStatus(200);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+};
