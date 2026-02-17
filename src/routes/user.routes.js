426100236a488000ae369051518ea31328cf2358
Pablo Basso <pablobasso25@gmail.com>
2026-02-17 17:45:44 -0300

    [WM-20] feat: agrega ruta para cambiar contraseña

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,10 +6,12 @@ import {
 } from "../controllers/user.controller.js";
 import { authRequired } from "../middlewares/validateToken.js";
 import { upload } from "../middlewares/upload.js";
+import { validateSchema } from "../middlewares/validator.middleware.js";
+import { changePasswordSchema } from "../schemas/user.schema.js";
 
 const router = Router();
 router.get("/profile", authRequired, profile);
 router.put("/profile", authRequired, upload.single("avatar"), updateProfile);
-router.put("/password", authRequired, changePassword);
+router.put("/password", authRequired, validateSchema(changePasswordSchema), changePassword);
 
 export default router;
