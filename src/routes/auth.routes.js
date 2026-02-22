dd7f3264d39d5637a5c5c4cd5c76af8d57d88c12
tomas <tomasgomezzi36@gmail.com>
2026-02-22 05:50:38 -0300

    [WM-40] feat: crea rutas para devolver la informacion en la validacion del cambio de clave

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -8,6 +8,7 @@ import {
 } from "../controllers/auth.controller.js";
 import { validateSchema } from "../middlewares/validator.middleware.js";
 import { loginSchema, registerSchema } from "../schemas/auth.schema.js";
+import { forgotPasswordSchema } from "../schemas/auth.schema.js";``
 
 const router = Router();
 router.post("/register", validateSchema(registerSchema), register);
@@ -15,5 +16,10 @@ router.post("/login", validateSchema(loginSchema), login);
 router.post("/logout", logout);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
+router.post(
+  "/forgot-password",
+  validateSchema(forgotPasswordSchema),
+  forgotPassword
+);
 
 export default router;
