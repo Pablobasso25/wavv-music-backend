953ec88820ad199a327774a46907c4727382dda6
Luana Jakubowicz <luanajakubowicz@hotmail.com>
2026-03-10 21:40:34 -0300

    Modificaciones base de datos

--- src/db.js
+++ src/db.js
@@ -1,5 +1,7 @@
 import { MONGODB_URI } from "./config.js";
 import mongoose from "mongoose";
+import Plan from "./models/plan.model.js";
+
 export const connectDB = async () => {
   try {
     if (!MONGODB_URI) {
@@ -8,9 +10,27 @@ export const connectDB = async () => {
       );
     }
     await mongoose.connect(MONGODB_URI);
-
     console.log(">>> DB is connected to Wavv Music <<<");
+    await seedPlans();
   } catch (error) {
     console.error("Error connecting to DB:", error.message);
+  }
+};
+
+const seedPlans = async () => {
+  try {
+    const count = await Plan.countDocuments();
+    if (count === 0) {
+      await Plan.create([
+        {
+          name: "Premium",
+          price: 6000,
+          benefits: ["Playlist Ilimitada", "Sin anuncios", "Calidad superior"],
+        },
+      ]);
+      console.log(">>> Solo se ha creado el Plan Premium <<<");
+    }
+  } catch (error) {
+    console.error("Error al crear planes:", error.message);
   }
 };
